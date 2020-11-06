ca75dbe9c84951a7f3d3582053426379b4ac95a6
Andrew Reynolds <andrew_reynolds@mac.com>
2020-11-05 17:34:22 -0800

    calculating ingredients length for showing message

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -2,16 +2,21 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = ({ ingredients }) => {
-  let ingredientsArray = Object.keys(ingredients).map((igKey) => {
-    return [...Array(ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
-  console.log(ingredientsArray);
+  let burgerIngredients = Object.keys(ingredients)
+    .map((igKey) => {
+      return [...Array(ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((acc, curr) => acc.concat(curr), []);
+  console.log(ingredients);
+  if (burgerIngredients.length === 0) {
+    burgerIngredients = <p>Please add ingredients to your burger</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type={"bread-top"} />
-      {ingredientsArray}
+      {burgerIngredients}
       <BurgerIngredient type={"bread-bottom"} />
     </div>
   );
