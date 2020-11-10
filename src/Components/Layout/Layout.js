11ede4e809c3aeba66ed0def76de3d29b1827d43
Andrew Reynolds <andrew_reynolds@mac.com>
2020-11-09 21:15:34 -0800

    created toolbar component

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const Layout = ({ children }) => {
   return (
     <Aux>
-      <div>Toolbar, SideDrawer, Backdrop</div>
+      <Toolbar />
       <main className={classes.Content}>{children}</main>
     </Aux>
   );
