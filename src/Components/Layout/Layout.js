8536d84b110c6d91bb526b32407193bb6262d214
Andrew Reynolds <andrew_reynolds@mac.com>
2020-11-10 12:03:27 -0800

    created sidedrawer component

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = ({ children }) => {
   return (
     <Aux>
       <Toolbar />
+      <SideDrawer />
       <main className={classes.Content}>{children}</main>
     </Aux>
   );
