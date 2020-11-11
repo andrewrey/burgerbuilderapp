bf2570fb4c74ebf4f65cb70f915bed2213abcdcf
Andrew Reynolds <andrew_reynolds@mac.com>
2020-11-11 13:25:27 -0800

    added shouldComponent update to modal

--- src/Components/Burger/OrderSummary/OrderSummary.js
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,36 @@
+import React, { Component } from "react";
 import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = ({ ingredients, modalClosed, continuePurchase, price }) => {
-  const ingredientSummary = Object.keys(ingredients).map((ing) => (
-    <li key={ing}>
-      <span style={{ textTransform: "capitalize" }}>{ing}</span>: {ingredients[ing]}
-    </li>
-  ));
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button clicked={modalClosed} btnType="Danger">
-        CANCEL
-      </Button>
-      <Button clicked={continuePurchase} btnType="Success">
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
+class orderSummary extends Component {
+  // componentDidUpdate() {
+  //   console.log("updated");
+  // }
+  render() {
+    let { ingredients, modalClosed, continuePurchase, price } = this.props;
+    const ingredientSummary = Object.keys(ingredients).map((ing) => (
+      <li key={ing}>
+        <span style={{ textTransform: "capitalize" }}>{ing}</span>: {ingredients[ing]}
+      </li>
+    ));
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button clicked={modalClosed} btnType="Danger">
+          CANCEL
+        </Button>
+        <Button clicked={continuePurchase} btnType="Success">
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
 
 export default orderSummary;
--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,22 +1,32 @@
+import React, { Component } from "react";
 import classes from "./Modal.module.css";
 import Aux from "../../../hoc/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = ({ children, show, modalClosed }) => {
-  return (
-    <Aux>
-      <Backdrop show={show} clicked={modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: show ? "1" : "0",
-        }}
-      >
-        {children}
-      </div>
-    </Aux>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  componentDidUpdate() {
+    console.log("Modal updated");
+  }
+  render() {
+    let { children, show, modalClosed } = this.props;
+    return (
+      <Aux>
+        <Backdrop show={show} clicked={modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: show ? "1" : "0",
+          }}
+        >
+          {children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
 export default Modal;
