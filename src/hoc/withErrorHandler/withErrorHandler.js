61ed4153baac7ef4acfaf10b75eca30396eab23e
Andrew Reynolds <andrew_reynolds@mac.com>
2020-11-20 23:00:57 -0800

    converted ewithErrorHandler to class component

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,14 +1,18 @@
+import React, { Component } from "react";
 import Modal from "../../Components/UI/Modal/Modal";
 import Aux from "../Aux/Aux";
 
 const withErrorHandler = (WrappedComponent) => {
-  return (props) => {
-    return (
-      <Aux>
-        <Modal>Something Didn't Work!</Modal>
-        <WrappedComponent {...props} />
-      </Aux>
-    );
+  return class extends Component {
+    componentDidMount() {}
+    render() {
+      return (
+        <Aux>
+          <Modal>Something Didn't Work!</Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      );
+    }
   };
 };
 
